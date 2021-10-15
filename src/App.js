bcd3f476b8ac4f181c3a4e2f16fa859a82bbba92
tringuyen1032 <trinmse140981@fpt.edu.vn>
2021-10-16 00:28:27 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ModelSearchPage from './containers/ModelSearchPage';
 import BrandSearchPage from './containers/BandSearchPage';
 import EventSearchPage from './containers/EventSearchPage';
 import ModelInfoPage from './containers/ModelInfoPage';
+import BrandInfoPage from './containers/BrandInfoPage';
 import { BrowserRouter, Route, Switch, Router } from 'react-router-dom';
 import NavBar from "./components/Header/NavBar";
 import SnackbarContent from "./components/Snackbar/SnackbarContent.js";
@@ -42,6 +43,7 @@ function App() {
             <Route path="/event-search/:id" component={EventSearchPage} />
             <Route path="/model-info/:id" component={ModelInfoPage} />
             <Route path="/event-create" component={CreateEventPage} />
+            <Route path="/brand-info" component={BrandInfoPage} />
          </Switch>
       </BrowserRouter>
    );
